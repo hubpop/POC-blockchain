63edc8bda1bfa5f3f3e84a06601a8ee9ad0e034a
173851 <hubert.popielarz@bzwbk.pl>
2019-10-15 17:34:22 +0200

    added validation of chain

--- src/main/java/com/example/SanChain/block/Block.java
+++ src/main/java/com/example/SanChain/block/Block.java
@@ -33,4 +33,6 @@ public class Block {
                 + transaction
         );
     }
+
+
 }
--- src/main/java/com/example/SanChain/chain/SanChain.java
+++ src/main/java/com/example/SanChain/chain/SanChain.java
@@ -8,5 +8,20 @@ public class SanChain {
 
     public static ArrayList<Block> blockchain = new ArrayList<>();
 
+    public static Boolean isChainValid(){
+        Block currentBlock;
+        Block previousBlock;
+
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+
+            if(!currentBlock.getPreviousHash().equals(previousBlock.getHash())){
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
